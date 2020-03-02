6fe420f0925b6377e87192fb395ab3a49e5fede2
Timz Owen <ktimothy@kabarak.ac.ke>
2020-03-02 11:44:35 +0300

    Update javascript_for_web.js
    mathematical operations and increments values

--- javascript_for_web.js
+++ javascript_for_web.js
@@ -27,3 +27,33 @@ c = c + " String!";
 
 //case sensitivity;
 // js is case sensitive and you should follow the camelCase during naming of your variables
+var myName;
+var properCamelCase;
+var intToAdd;
+
+myName = "Owen";
+properCamelCase = "String";
+intToAdd = 15;
+
+//basic mathematical operations with JavaScript
+myVarSum = 5 + 15;
+additionOfTwoNumbers = 10 + 5 + 2;
+var difference = 45 - 23;
+var product = 10 * 10;
+var quotient = 66 / 33;
+
+//basic variables increment and decrement values using ++ operator;
+//i++ is the same as i = i+1;
+var myNumber;
+myNumber = myNumber + 1;
+myNumber++;
+i = i + 1; // same as i++;
+
+var myVar = 10;
+myVar = myVar - 1;
+myVar--;
+
+//Decimal numbers with javascript/ floating numbers
+var myDecimal = 10.56;
+var decimalMultiplication = 2.5 * 2.5;
+var decimalDivision = 10.2 / 2.5;

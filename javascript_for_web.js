78f3bda8c493ffd8cd1666b61bd1c0d58a615e42
Timz Owen <ktimothy@kabarak.ac.ke>
2020-03-02 10:45:16 +0300

    Create javascript_for_web.js
    initial commit

---
+++ javascript_for_web.js
@@ -0,0 +1,29 @@
+//comments in js use double forward slash
+/*
+This is also comment
+*/
+
+//variable in javascript
+//uses key words var to declare variables and ends in semicolon
+//also uses assignment operator to assign values
+var myName;
+var a;
+var b = 7;
+b = a;
+console.log(a); //now prints the values of a assigned to b;
+
+//varible initialization with assignment operator
+var a = 10;
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a ";
+
+// assign task here 
+
+a = a + 2;
+b = b + 7;
+c = c + " String!";
+
+//case sensitivity;
+// js is case sensitive and you should follow the camelCase during naming of your variables

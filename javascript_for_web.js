7c35ae17171234a044c64f3de2d95e94350949f0
Timz Owen <ktimothy@kabarak.ac.ke>
2020-03-06 13:41:59 +0300

    Update javascript_for_web.js
    full switch cases

--- javascript_for_web.js
+++ javascript_for_web.js
@@ -978,11 +978,12 @@ console.log(golfScore(8, 2)); // Eagle
 console.log(golfScore(2, 10)); // Go Home
 
 
-//UPNEXT: using Switch statements:
 //UP -NEXT: using Switch statements:
 //Switch enables multiple selection choice.executions continues until BREAK statement is reached where execution stops
 //Switch statements are strictly executed with === and break tells js to stop executing the statement at hand
 //adding of default values in switch case; useful incase there is no  matching case in the statement
+//multi-identical  options: if you have same output, then we can switch them as: 
+//replacing the if else statements with switch statements;
 switch (educationLevel) {
     case "kid":
         console.log("Nursery");
@@ -1047,3 +1048,154 @@ switch (mpesa) {
         defaultStatement;
         break;
 }
+
+function switchOfStuff(val) {
+    var answer = "";
+    switch (val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff"
+            break;
+    }
+    return answer;
+}
+
+console.log(switchOfStuff("a")); // apple
+
+
+function checkGrades(score) {
+    var yourCommets = "";
+    switch (score) {
+        case "A":
+            yourCommets = "Excellent";
+            break;
+        case "B":
+            yourCommets = "Good";
+            break;
+        case "C":
+            yourCommets = "poor";
+            break;
+        case "D":
+            yourCommets = "Failure";
+        case "F":
+            yourCommets = "Failure";
+            break;
+        default:
+            yourCommets = "No Such Grade";
+            break;
+    }
+    return yourCommets;
+}
+console.log(checkGrades("G")); //no such grade
+
+//multiple-identical options
+switch (samValues) {
+    case 1:
+    case 2:
+    case 3:
+        result = "1, 2, or 3";
+        break;
+    case 4:
+        result = "4 alone";
+}
+
+function sequentialSizes(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+        default:
+            answer = "out of loop";
+            break;
+    }
+    return answer;
+}
+
+console.log(sequentialSizes(5)); //mid
+console.log(sequentialSizes(1)); // low
+console.log(sequentialSizes(8)); //High
+
+//change to switch
+if (val === 1) {
+    answer = "a";
+} else if (val === 2) {
+    answer = "b";
+} else {
+    answer = "c";
+} //replaced with
+
+switch (val) {
+    case 1:
+        answer = "a";
+        break;
+    case 2:
+        answer = "b";
+        break;
+    default:
+        answer = "c";
+}
+//Ex 2
+function chainToSwitch(val) {
+    var answer = "";
+    if (val === "bob") {
+        answer = "Marley";
+    } else if (val === 42) {
+        answer = "The Answer";
+    } else if (val === 1) {
+        answer = "There is no #1";
+    } else if (val === 99) {
+        answer = "Missed me by this much!";
+    } else if (val === 7) {
+        answer = "Ate Nine";
+    }
+    return answer;
+}
+console.log(chainToSwitch(7)); //At nine
+//replaced with:
+function chainToSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    return answer;
+}
+
+console.log(chainToSwitch(7));
+
+
+//UP-NEXT : Returning boolean values from functions;

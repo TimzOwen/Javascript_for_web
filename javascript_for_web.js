77f89fbeb7662c67a110b81286143bee88607425
Timz Owen <ktimothy@kabarak.ac.ke>
2020-03-08 12:38:59 +0300

    Update javascript_for_web.js
    Game arrays names[] updated . Debugging

--- javascript_for_web.js
+++ javascript_for_web.js
@@ -956,7 +956,7 @@ var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", 
 
 function golfScore(par, strokes) {
     if (strokes == 1) {
-        return name[0];
+        return names[0];
     } else if (strokes <= par - 2) {
         return names[1];
     } else if (strokes == par - 1) {
